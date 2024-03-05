8ef7e110574d4b2c1b2e624db8b5b7ccb322babe
akashkakade26 <kakade.akash5455@gmail.com>
2024-03-05 20:10:37 +0530

    Add project files.

---
+++ CalCulatorDlg.h
@@ -0,0 +1,33 @@
+
+// CalCulatorDlg.h : header file
+//
+
+#pragma once
+
+
+// CCalCulatorDlg dialog
+class CCalCulatorDlg : public CDialogEx
+{
+// Construction
+public:
+	CCalCulatorDlg(CWnd* pParent = nullptr);	// standard constructor
+
+// Dialog Data
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_CALCULATOR_DIALOG };
+#endif
+
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);	// DDX/DDV support
+
+
+// Implementation
+protected:
+	HICON m_hIcon;
+
+	// Generated message map functions
+	virtual BOOL OnInitDialog();
+	afx_msg void OnPaint();
+	afx_msg HCURSOR OnQueryDragIcon();
+	DECLARE_MESSAGE_MAP()
+};
