7205b1fab1323ca79bd6ac87a2a229b29160346d
akashkakade26 <kakade.akash5455@gmail.com>
2024-03-08 22:31:49 +0530

    08032024

--- Resource.h
+++ Resource.h
@@ -23,14 +23,32 @@
 #define ID_BT_CLEAR                     1017
 #define IDC_EDIT1                       1018
 #define ID_EB_RESULT_DISPLAY            1018
+#define ID_MFC_BT_0                     1020
+#define ID_MFC_BT_1                     1021
+#define ID_MFC_BT_2                     1022
+#define ID_MFC_BT_3                     1023
+#define ID_MFC_BT_4                     1024
+#define ID_MFC_BT_5                     1025
+#define ID_MFC_BT_6                     1026
+#define ID_MFC_BT_7                     1027
+#define ID_MFC_BT_8                     1028
+#define ID_MFC_BT_9                     1029
+#define ID_MFC_BT_DOT                   1030
+#define ID_MFC_BT_RESULT                1031
+#define ID_MFC_BT_ADD                   1032
+#define ID_MFC_BT_SUBSTRACT             1033
+#define ID_MFC_BT_MULTIPLY              1034
+#define ID_MFC_BT_DIVIDE                1035
+#define ID_MFC_BT_10                    1036
+#define ID_MFC_BT_CLEAR                 1036
 
 // Next default values for new objects
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE        130
+#define _APS_NEXT_RESOURCE_VALUE        131
 #define _APS_NEXT_COMMAND_VALUE         32771
-#define _APS_NEXT_CONTROL_VALUE         1019
+#define _APS_NEXT_CONTROL_VALUE         1021
 #define _APS_NEXT_SYMED_VALUE           101
 #endif
 #endif
